a140a635855db32f1f1366f680b331a82a42762a
Julio Nascimento <julio.nascimento@synus.com.br>
2019-04-23 18:48:17 -0300

    Exibindo a idade

--- public/index.php
+++ public/index.php
@@ -1,1 +1,13 @@
-Hello World
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$date = new \DateTime;
+$date->setDate(1991, 10, 11);
+$today = new \DateTime;
+
+$age = new \App\Age($date, $today);
+
+?>
+
+Minha Idade = <?=$age;?>
